2fdce499a8c72f478a290cfd0d8f01750544d324
mckasza <mckasza87@gmail.com>
2021-05-05 16:36:43 -0500

    Created ClientThread class
    - Created ClientThread class to handle threading of client connections

--- ftp_server.py
+++ ftp_server.py
@@ -1,4 +1,3 @@
-import tkinter as tk
 import socket
 import threading
 import time
@@ -6,61 +5,54 @@ import select
 import pathlib
 import stat
 import datetime
-			
-try:
-	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server_socket.setblocking(False)
-	server_socket.bind(('127.0.0.1', 21))
-	server_socket.listen()
 
-	while True:
-		readable = select.select([server_socket], [], [], 0.25)[0]
-		
-		if server_socket in readable:
-			conn, address = server_socket.accept()
+class ClientThread(threading.Thread):
+	def __init__(self, client_connection, address, stop_event):
+		super().__init__(daemon=True)
+		self.client_connection = client_connection
+		self.stop_event = stop_event
+	
+	def run(self):
+		self.client_connection.sendall(b'220 (FTP Server)\r\n')
+		passive_conn = None
+		passive_address = None
+	
+		while True:
+			readable = select.select([self.client_connection], [], [], 0.1)[0]
 			
-			conn.sendall(b'220 (FTP Server)\r\n')
-			passive_conn = None
-			passive_address = None
-			
-			while True:
-				readable, writeable, exception = select.select([conn], [], [], 0.25)
-				
-				if conn in readable:
-					data = conn.recv(1024)
-					if len(data) == 0:
-						break
-						
-					if data[0:4] == b'USER':
-						conn.sendall(b'331 Please specifiy the password.\r\n')
-					if data[0:4] == b'PASS':
-						conn.sendall(b'230 Login Successful.\r\n')
-					elif data[0:4] == b'SYST':
-						conn.sendall(b'215 UNIX Type: L8\r\n')
-					elif data[0:3] == b'PWD':
-						conn.sendall(b'257 "/"\r\n')
-					elif data[0:6] == b'TYPE I':
-						conn.sendall(b'200 Switching to Binary mode.\r\n')
-					elif data[0:4] == b'SIZE':
-						conn.sendall(b'550 Could not get file size.\r\n')
-					elif data[0:3] == b'CWD':
-						conn.sendall(b'250 Directory successfully changed.\r\n')
-					elif data[0:4] == b'PASV':
-						passive_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-						passive_socket.settimeout(15)
-						passive_socket.bind(('127.0.0.1', 0))
-						
-						port = passive_socket.getsockname()[1]
-						print(port)
-						port_high = int(port/256)
-						port_low = port-256*int(port/256)
-						
-						conn.sendall(bytes('227 Entering Passive Mode (127,0,0,1,{},{}).\r\n'.format(port_high, port_low), 'utf-8'))
-						
-						passive_socket.listen()
-						passive_conn, passive_address = passive_socket.accept()
-					elif data[0:4] == b'LIST':
-						conn.sendall(b'150 Here comes the directory listing.\r\n')
+			if self.client_connection in readable:
+				data = self.client_connection.recv(1024)
+				if len(data) == 0:
+					break
+					
+				if data[0:4] == b'USER':
+					self.client_connection.sendall(b'331 Please specifiy the password.\r\n')
+				if data[0:4] == b'PASS':
+					self.client_connection.sendall(b'230 Login Successful.\r\n')
+				elif data[0:4] == b'SYST':
+					self.client_connection.sendall(b'215 UNIX Type: L8\r\n')
+				elif data[0:3] == b'PWD':
+					self.client_connection.sendall(b'257 "/"\r\n')
+				elif data[0:6] == b'TYPE I':
+					self.client_connection.sendall(b'200 Switching to Binary mode.\r\n')
+				elif data[0:4] == b'SIZE':
+					self.client_connection.sendall(b'550 Could not get file size.\r\n')
+				elif data[0:3] == b'CWD':
+					self.client_connection.sendall(b'250 Directory successfully changed.\r\n')
+				elif data[0:4] == b'PASV':
+					passive_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+					passive_socket.bind(('127.0.0.1', 0))
+					
+					port = passive_socket.getsockname()[1]
+					port_high = int(port/256)
+					port_low = port-256*int(port/256)
+					
+					self.client_connection.sendall(bytes('227 Entering Passive Mode (127,0,0,1,{},{}).\r\n'.format(port_high, port_low), 'utf-8'))
+					
+					passive_socket.listen()
+					passive_conn, passive_address = passive_socket.accept()
+				elif data[0:4] == b'LIST':
+						self.client_connection.sendall(b'150 Here comes the directory listing.\r\n')
 						
 						directory_listing = []
 						for item in pathlib.Path.cwd().iterdir():
@@ -79,10 +71,48 @@ try:
 						passive_conn.sendall(bytes('\r\n'.join(directory_listing)+'\r\n', 'utf-8'))
 						passive_conn.close()
 						
-						conn.sendall(b'226 Directory send OK.\r\n')
+						self.client_connection.sendall(b'226 Directory send OK.\r\n')
+				elif data[0:4] == b'QUIT':
+					self.client_connection.close()
+					break
 						
-	server_socket.close()
+			if self.stop_event.is_set():
+				if passive_conn:
+					passive_conn.close()
+				break
+				
+		
+		
+try:
+	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server_socket.setblocking(False)
+	server_socket.bind(('127.0.0.1', 21))
+	server_socket.listen()
+
+	stop_event = threading.Event()
+	
+	client_threads = []
+	
+	while True:
+		readable = select.select([server_socket], [], [], 0.1)[0]
+		
+		if server_socket in readable:
+			conn, address = server_socket.accept()
+			
+			client_thread = ClientThread(conn, address, stop_event)
+			client_threads.append(client_thread)
+			client_thread.start()
+			
+		for thread in client_threads:
+			if thread.is_alive():
+				thread.join(0.1)
 			
 except KeyboardInterrupt:
+	stop_event.set()
+	
+	for thread in client_threads:
+		if thread.is_alive():
+			thread.join()
+		
 	server_socket.close()
 		
